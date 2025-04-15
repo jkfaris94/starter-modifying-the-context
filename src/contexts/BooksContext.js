9fa4dc0a33658ce888b586b55cc1fe6e0c7d74f4
Johnny Faris <jkfaris@gmail.com>
2025-04-15 16:02:46 -0700

    updated context file to use useState and createContext with initialData and children

--- src/contexts/BooksContext.js
+++ src/contexts/BooksContext.js
@@ -1,2 +1,15 @@
-import { createContext } from "react";
-export const BooksContext = createContext([]);
+import { createContext, useState } from "react";
+
+export const BooksContext = createContext({
+    books: [],
+    setBooks: () = {},
+});
+
+export function BooksContextProvider({ initialData, children }) {
+    const [books, setBooks] = useState(initialData);
+    return (
+        <BooksContext.Provider value={{ books, setBooks }}>
+            {children}
+        </BooksContext.Provider>
+    );
+}
